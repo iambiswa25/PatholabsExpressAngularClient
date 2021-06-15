c6ee8e5c0a54de2c6f94f4d294ab218a2f7e2866
Apoorv2010 <apoorvjain6@gmail.com>
2021-06-15 11:13:53 +0530

    HomePage changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,20 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import {HttpClientModule} from '@angular/common/http';
+import { HomePageComponent } from './home-page/home-page.component';
+import { FooterComponent } from './home-page/footer/footer.component';
+import { HeaderComponent } from './home-page/header/header.component';
+import { MainComponent } from './home-page/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    HomePageComponent,
+    FooterComponent,
+    HeaderComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
