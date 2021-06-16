c9db8e3215a4b710b389624befec545b619ec759
Biswaranjan Ghadai <biswaranjan.ghadai@bipros.com>
2021-06-16 21:14:50 +0530

    Biswa-- updated client side

--- src/app/shared/service/common.service.ts
+++ src/app/shared/service/common.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { serverResponse } from 'src/app/entity/common/response';
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +14,8 @@ export class CommonService {
   constructor(private http: HttpClient) {
     this.actionUrl = this.serverBaseUrl + this.api;
   }
-  public login(loginvalue: any): Observable<Response> {
-    let actionUrl = this.actionUrl + 'User_Admins';
-    return this.http.post<Response>(actionUrl, JSON.stringify(loginvalue))
+  public login(loginvalue: any): Observable<serverResponse> {
+    let actionUrl = this.actionUrl + 'Login';
+    return this.http.post<serverResponse>(actionUrl, JSON.stringify(loginvalue))
   }
 }
