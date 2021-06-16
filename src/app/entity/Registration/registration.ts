59528622208e77cbcfb1a7c2f4648cfd8dbb0609
Satya Prakash Sahoo <satyaprakash.sahoo@bipros.com>
2021-06-16 22:35:49 +0530

    SATYA -- added regddto

---
+++ src/app/entity/Registration/registration.ts
@@ -0,0 +1,13 @@
+export class UserDto
+    {
+        public Id!:number;
+        public  UserId! :number;
+        public  Name! :string;
+        public  Address! :string;
+        public  Age?:number;
+        public  Gender! :string;
+        public  Email! :string;
+        public  Contact_No! :string;
+        public  Password! :string;
+        public  UserType!:string;
+    }
